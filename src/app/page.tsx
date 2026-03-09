a93e224b70f28d1f76378bef97925ce8dd349061
Manishtripathi-108 <manish82644@gmail.com>
2026-03-09 06:25:38 -0400

    feat: replace AnimatedThemeToggler with ThemeToggler component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { AnimatedThemeToggler } from "@/components/ui/ThemeToggler";
+import ThemeToggler from "@/components/ui/ThemeToggler";
 import Image from "next/image";
 
 export default function Home() {
 	return (
 		<div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-			<AnimatedThemeToggler className='absolute top-4 right-4 cursor-pointer' />
+			<ThemeToggler className='absolute top-4 right-4 cursor-pointer' />
 			<main className='flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start'>
 				<Image className='dark:invert' src='/next.svg' alt='Next.js logo' width={100} height={20} priority />
 				<div className='flex flex-col items-center gap-6 text-center sm:items-start sm:text-left'>
